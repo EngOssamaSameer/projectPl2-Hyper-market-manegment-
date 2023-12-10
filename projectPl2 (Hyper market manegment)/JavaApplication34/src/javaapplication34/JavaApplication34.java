8f6ce07f57d13c1252663ad285f0aa53b2544adb
Ossama Samir <147268769+EngOssamaSameer@users.noreply.github.com>
2023-12-10 07:07:55 +0200

    OOP

---
+++ projectPl2 (Hyper market manegment)/JavaApplication34/src/javaapplication34/JavaApplication34.java
@@ -0,0 +1,24 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package javaapplication34;
+
+import java.io.File;
+import java.util.Scanner;
+
+/**
+ *
+ * @author TECH SHOP
+ */
+public class JavaApplication34 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       Number numberRef = new Integer(0);
+       Double doubleRef = (Double)numberRef;
+    }
+    
+}
